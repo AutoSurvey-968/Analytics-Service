89495f800fc93c45f8149b29a37394e4844caa92
kevinrose235 <kevinrose235@gmail.com>
2021-05-25 10:26:57 -0500

    reverted to old dao

--- src/main/java/com/revature/autosurvey/analytics/data/ResponseDaoImpl.java
+++ src/main/java/com/revature/autosurvey/analytics/data/ResponseDaoImpl.java
@@ -1,16 +1,11 @@
 package com.revature.autosurvey.analytics.data;
 
-import java.util.HashMap;
-import java.util.Map;
-import java.util.UUID;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.client.WebClient;
 
 import com.revature.autosurvey.analytics.beans.Response;
-import com.revature.autosurvey.analytics.beans.Response.WeekNum;
 
 import reactor.core.publisher.Flux;
 
@@ -25,34 +20,13 @@ public class ResponseDaoImpl implements ResponseDao {
 
 	@Override
 	public Flux<Response> getResponses(String surveyId) {
-//		WebClient wc = webClient.baseUrl(env.getProperty("GATEWAY_URL")).build();
-//		Flux<Response> response = wc.get()
-//				.uri(uriBuilder -> uriBuilder.path("/responses/{surveyId}")
-//				.build(surveyId))
-//				.retrieve()
-//				.bodyToFlux(Response.class);
-//		return response;
-		Map<String, String> surveyResponse1 = new HashMap<>();
-		surveyResponse1.put("test", "1");
-		Response testResponse1 = new Response(
-				UUID.randomUUID(),
-				"test batchname", 
-				WeekNum.A, 
-				UUID.randomUUID(), 
-				surveyResponse1);
-		
-		Map<String, String> surveyResponse2 = new HashMap<>();
-		surveyResponse2.put("test", "2");
-		Response testResponse2 = new Response(
-				UUID.randomUUID(), 
-				"test batchname", 
-				WeekNum.A, 
-				UUID.randomUUID(), 
-				surveyResponse2);
-		
-		
-		return Flux.just(testResponse1, testResponse2);
-
+		WebClient wc = webClient.baseUrl(env.getProperty("GATEWAY_URL")).build();
+		Flux<Response> response = wc.get()
+				.uri(uriBuilder -> uriBuilder.path("/responses/{surveyId}")
+				.build(surveyId))
+				.retrieve()
+				.bodyToFlux(Response.class);
+		return response;
 	}
 
 	@Override
--- src/main/java/com/revature/autosurvey/analytics/data/SurveyDaoImpl.java
+++ src/main/java/com/revature/autosurvey/analytics/data/SurveyDaoImpl.java
@@ -1,17 +1,10 @@
 package com.revature.autosurvey.analytics.data;
 
-import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.UUID;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.client.WebClient;
 
-import com.revature.autosurvey.analytics.beans.Question;
-import com.revature.autosurvey.analytics.beans.Question.QuestionType;
 import com.revature.autosurvey.analytics.beans.Survey;
 
 import reactor.core.publisher.Mono;
@@ -27,35 +20,14 @@ public class SurveyDaoImpl implements SurveyDao {
 
 	@Override
 	public Mono<Survey> getSurvey(String surveyId) {
-//		WebClient wc = webClient.baseUrl(env.getProperty("GATEWAY_URL")).build();
-//		Mono<Survey> survey = wc.get()
-//				.uri(uriBuilder -> uriBuilder.path("/survey/{surveyId}")
-//				.build(surveyId))
-//				.retrieve()
-//				.bodyToMono(Survey.class);
-//		return survey;
-		List<Question> questionsList = new ArrayList<Question>();
-		Question question = new Question();
-		question.setQuestionType(QuestionType.MULTIPLE_CHOICE);
-		question.setTitle("test");
-		question.setHelpText("test help text");
-		question.setIsRequired(true);
-		List<String> choices = new ArrayList<String>();
-		choices.add("1");
-		choices.add("2");
-		question.setChoices(choices);
-		question.setHasOtherOption(false);
-		questionsList.add(question);
-		return Mono.just(new Survey(
-				UUID.randomUUID(), 
-				LocalDateTime.now(), 
-				"test survey title", 
-				"test description", 
-				"test confirmation", 
-				"test version", 
-				new ArrayList<String>(), 
-				questionsList)
-				);
+		WebClient wc = webClient.baseUrl(env.getProperty("GATEWAY_URL")).build();
+		Mono<Survey> survey = wc.get()
+				.uri(uriBuilder -> uriBuilder.path("/survey/{surveyId}")
+				.build(surveyId))
+				.retrieve()
+				.bodyToMono(Survey.class);
+		return survey;
+
 	}
 
 }
