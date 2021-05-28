4f8088837f9d269598abc78663ad724e15e83021
DuskDaleSpider <dakota.ray.clark@gmail.com>
2021-05-28 15:22:50 -0400

    Updated run-docker

--- docker/run-docker.sh
+++ docker/run-docker.sh
@@ -15,5 +15,3 @@ fi
 docker container run -d --name gateway-service --network autosurvey-network \
     -p 80:8080 -e EUREKA_URL -e CREDENTIALS_JSON -e CREDENTIALS_JSON_ENCODED \ 
     -e FIREBASE_API_KEY -e SERVICE_ACCOUNT_ID autosurvey/analytics-service
-
-#Changed permission
