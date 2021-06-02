e983be0c53fcf4cd4e7bdce74e68d1ab2536ec1b
awithers8 <austin8withers@gmail.com>
2021-06-02 12:30:06 -0400

    Fix Response

--- src/main/java/com/revature/autosurvey/analytics/beans/Response.java
+++ src/main/java/com/revature/autosurvey/analytics/beans/Response.java
@@ -1,64 +1,78 @@
 package com.revature.autosurvey.analytics.beans;
 
+import java.util.Date;
 import java.util.Map;
 import java.util.UUID;
 
 public class Response {
 
-	private UUID responseId;
-	private String batchName;
+	private UUID uuid;
+	private String batch;
 
-	private String week;
-	private UUID surveyId;
-	private Map<String, String> surveyResponses;
+	private Date date;
+	private UUID surveyUuid;
+	private Map<String, String> responses;
 
 	public Response(UUID responseId, String batchName, String week, UUID surveyId,
 			Map<String, String> surveyResponses) {
 		super();
-		this.responseId = responseId;
-		this.batchName = batchName;
-		this.week = week;
-		this.surveyId = surveyId;
-		this.surveyResponses = surveyResponses;
-	}
-
-	public UUID getResponseId() {
-		return responseId;
-	}
-
-	public void setResponseId(UUID responseId) {
-		this.responseId = responseId;
-	}
-
-	public String getBatchName() {
-		return batchName;
-	}
-
-	public void setBatchName(String batchName) {
-		this.batchName = batchName;
-	}
-
-	public String getWeek() {
-		return week;
-	}
-
-	public void setWeek(String week) {
-		this.week = week;
+		this.surveyUuid = surveyId;
+		this.responses = surveyResponses;
 	}
 
 	public UUID getSurveyId() {
-		return surveyId;
+		return surveyUuid;
 	}
 
 	public void setSurveyId(UUID surveyId) {
-		this.surveyId = surveyId;
+		this.surveyUuid = surveyId;
 	}
 
 	public Map<String, String> getSurveyResponses() {
-		return surveyResponses;
+		return responses;
 	}
 
 	public void setSurveyResponses(Map<String, String> surveyResponses) {
-		this.surveyResponses = surveyResponses;
+		this.responses = surveyResponses;
+	}
+
+	public UUID getUuid() {
+		return uuid;
+	}
+
+	public void setUuid(UUID uuid) {
+		this.uuid = uuid;
+	}
+
+	public String getBatch() {
+		return batch;
+	}
+
+	public void setBatch(String batch) {
+		this.batch = batch;
+	}
+
+	public Date getDate() {
+		return date;
+	}
+
+	public void setDate(Date date) {
+		this.date = date;
+	}
+
+	public UUID getSurveyUuid() {
+		return surveyUuid;
+	}
+
+	public void setSurveyUuid(UUID surveyUuid) {
+		this.surveyUuid = surveyUuid;
+	}
+
+	public Map<String, String> getResponses() {
+		return responses;
+	}
+
+	public void setResponses(Map<String, String> responses) {
+		this.responses = responses;
 	}
 }
