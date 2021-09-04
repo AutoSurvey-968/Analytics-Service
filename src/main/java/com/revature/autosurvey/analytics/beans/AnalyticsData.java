aee229cd61f7c76581e4d0b45819b0dac032088a
MuckJosh <joshuamuckey@gmail.com>
2021-09-04 16:21:00 -0400

    the changes to make it work

--- src/main/java/com/revature/autosurvey/analytics/beans/AnalyticsData.java
+++ src/main/java/com/revature/autosurvey/analytics/beans/AnalyticsData.java
@@ -1,7 +1,13 @@
 package com.revature.autosurvey.analytics.beans;
 
 import lombok.Data;
-
+/**
+ * 
+ * @author MuckJosh
+ * Data class to hold averages and deltas for reports and questions
+ * Datum is considered for percentages and averages for the responses.
+ * Delta is the change between reports for those averages/percentages.
+ */
 @Data
 public class AnalyticsData {
 	private double datum;
