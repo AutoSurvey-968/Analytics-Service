374cdd596593ad4db728692ea2d6f307a09e1ba7
siddmohanty111 <siddhant_99@hotmail.com>
2021-09-08 08:21:39 -0700

    voic

--- src/main/java/com/revature/autosurvey/analytics/utils/MessageReceiver.java
+++ src/main/java/com/revature/autosurvey/analytics/utils/MessageReceiver.java
@@ -2,15 +2,15 @@ package com.revature.autosurvey.analytics.utils;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.aws.messaging.listener.SqsMessageDeletionPolicy;
 import org.springframework.cloud.aws.messaging.listener.annotation.SqsListener;
 import org.springframework.messaging.Message;
 import org.springframework.stereotype.Component;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.amazonaws.util.json.Jackson;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.revature.autosurvey.analytics.beans.Survey;
 
 import lombok.extern.slf4j.Slf4j;
@@ -28,8 +28,8 @@ public class MessageReceiver {
 		mapper = new ObjectMapper();
 	}
 	
-	@SqsListener(value = SQSQueueNames.SURVEY_QUEUE, S)
-	public voic receiveMessageSurvey(Message<String> message) {
+	@SqsListener(value = SQSQueueNames.SURVEY_QUEUE, deletionPolicy=SqsMessageDeletionPolicy.ON_SUCCESS)
+	public void receiveMessageSurvey(Message<String> message) {
 		messageData.add(Jackson.fromJsonString(message.getPayload(), Survey.class)); 
 	}
 	
